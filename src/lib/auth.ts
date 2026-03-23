aa437752f02e29289a5117a4606a53c338c48b3c
Pratik Kulkarni <pnkulkarni@proton.me>
2026-03-23 12:30:29 +0530

    chore(fix): added tiny changes which include logging 5xx errors in App and using connection string instead of separate configuration items in auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,17 +3,15 @@ import {Pool} from "pg";
 
 export const auth = betterAuth({
     database: new Pool({
-        user: process.env.DATABASE_USER,
-        host: process.env.DATABASE_HOST,
-        database: process.env.DATABASE_NAME,
-        password: process.env.DATABASE_PASSWORD,
-        port: Number(process.env.DATABASE_PORT),
+        connectionString: process.env.DATABASE_URL!,
+        // user: process.env.DATABASE_USER,
+        // host: process.env.DATABASE_HOST,
+        // database: process.env.DATABASE_NAME,
+        // password: process.env.DATABASE_PASSWORD,
+        // port: Number(process.env.DATABASE_PORT),
     }),
     trustedOrigins: [process.env.FRONTEND_URL!, "http://localhost:3000"],
     emailAndPassword: {enabled: true},
-    // plugins: [
-    //     openAPI()
-    // ],
     session: {
         expiresIn: 7 * 24 * 60 * 60,
         cookieCache: {enabled: true, maxAge: 5 * 60},
