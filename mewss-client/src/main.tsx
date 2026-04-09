a4a77d0a7070994a3f5b2a7141eedd1de88daa02
Pratik Kulkarni <pnkulkarni@proton.me>
2026-04-09 06:06:16 +0000

    feat(router) - add router and relevant configuration for tanstack router

--- mewss-client/src/main.tsx
+++ mewss-client/src/main.tsx
@@ -1,14 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './lib/query-client.ts'
+import {QueryClientProvider} from '@tanstack/react-query'
+import {queryClient} from './lib/query-client.ts'
+import {createRouter, RouterProvider} from "@tanstack/react-router";
+import {routeTree} from "./routeTree.gen.ts"
+
+const router = createRouter({
+    routeTree,
+    context: {
+        queryClient
+    },
+    defaultPreload: "intent",
+    defaultPreloadStaleTime: 0,
+})
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>,
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router}/>
+        </QueryClientProvider>
+    </StrictMode>,
 )
---
+++ mewss-client/src/routes/__root.tsx
@@ -0,0 +1,29 @@
+import type {QueryClient} from "@tanstack/react-query";
+import {createRootRouteWithContext, Outlet} from "@tanstack/react-router";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {TanStackRouterDevtools} from "@tanstack/react-router-devtools";
+
+interface RouterContext {
+    queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<RouterContext>()({
+    component: RootComponent
+})
+
+function RootComponent() {
+
+    return (
+        <>
+            <Outlet/>
+
+            {/* TODO: make this based off an env variable */}
+            {(
+                <>
+                    <ReactQueryDevtools initialIsOpen={false}/>
+                    <TanStackRouterDevtools position="bottom-left"/>
+                </>
+            )}
+        </>
+    )
+}
