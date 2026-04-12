d01190252c5656086d4320fc38e9c08a01bfe599
Pratik Kulkarni <pnkulkarni@proton.me>
2026-04-12 12:28:32 +0000

    feat(frontend): update sidebar configuration in the home page

--- mewss-client/src/routes/_app/home/index.tsx
+++ mewss-client/src/routes/_app/home/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { SidebarProvider, SidebarTrigger } from '../../../components/ui/sidebar'
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '../../../components/ui/sidebar'
 import { AppSidebar } from '../../../components/ui/app-sidebar'
 
 export const Route = createFileRoute('/_app/home/')({
@@ -10,8 +10,20 @@ function HomeComponent() {
   return (
     <div>
       <SidebarProvider>
-        <AppSidebar />
-        <SidebarTrigger />
+        <SidebarInset className="flex flex-row overflow-hidden"></SidebarInset>
+        <div className="flex h-screen w-full overflow-hidden bg-card text-foreground font-sans">
+          <AppSidebar />
+          <div className="flex-none w-[380px] bg-card border-r border-border flex flex-col h-full overflow-hidden">
+            <header className="relative flex-none h-14 border-b border-border flex items-center px-5 glass sticky top-0 z-10">
+              <div className="flex items-center">
+                <SidebarTrigger />
+              </div>
+              <h2 className="absolute left-1/2 -translate-x-1/2 text-[15px] font-semibold">
+                All Articles
+              </h2>
+            </header>
+          </div>
+        </div>
       </SidebarProvider>
     </div>
   )
